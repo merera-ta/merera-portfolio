f41453b76f3c18028e90f74a0f46613c80c10d87
Merara <mererataddesa1@gmail.com>
2026-08-14 17:48:06 +0300

    update portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { Routes, Route } from 'react-router-dom'
-import { useEffect } from 'react'
-import Home from './pages/Home.jsx'
-import { useTheme } from './hooks/useTheme.js'
+import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import Home from "./pages/Home.jsx";
+import { useTheme } from "./hooks/useTheme.js";
 
 function App() {
   // Initialize theme as early as possible so the whole app renders themed.
-  useTheme()
+  useTheme();
 
   useEffect(() => {
-    document.title = 'Merera Taddesa | MERN Stack Developer'
-  }, [])
+    document.title = " | MERN Stack Developer";
+  }, []);
 
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="*" element={<Home />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
